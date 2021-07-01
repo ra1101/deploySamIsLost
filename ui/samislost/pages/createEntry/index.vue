6083f345ba7a6bc320015950b651f2695beff501
Ray Archer <s.r.archer@gmail.com>
2021-07-01 06:21:13 -0400

    adding source files to deploy repo

---
+++ ui/samislost/pages/createEntry/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Sam Is Lost</h2>
+    <p>Tell Us About Your Animal Sighting</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "SamIsLost New Sighting",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best way to get animals back home",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
